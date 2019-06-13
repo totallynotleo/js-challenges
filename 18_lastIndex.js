4d70ff56a83e1711f012b51574ae9748767c6e45
totallynotleo <leodabrim@gmail.com>
2019-06-13 13:57:22 +1000

    Refactored code, still doesn't work

--- 18_lastIndex.js
+++ 18_lastIndex.js
@@ -18,24 +18,46 @@ _.findLastIndex(users, {
 => 3
 */
 
+// for (let i = 0; i < array.length; i++) {
+//     if (reversedArray[i] == needle) {
+//         return array.findIndex(reversedArray[i]);
+//     } else if (reversedArray[i].needle) {
+//         return array.findIndex(reversedArray[i]);
+//     }
+// }
 const findLastIndex = (array, needle) => {
-// Your code here!
-}
-  
-  
+    let reversedArray = array.reverse();
+    let keyToFind = Object.keys(needle)[0];
+    let valueToFind = Object.values(needle)[0];
+    object = { keyToFind: valueToFind };
+    for (let i = 0; i < reversedArray.length; i++) {
+        for (key in reversedArray[i]) {
+            if (key == keyToFind) {
+                obj.value = valueToFind;
+                return array.findIndex(reversedArray[i]);
+            }
+        }
+    }
+    return null;
+};
+
 // Check your solution by running these tests: mocha last_index.js
 const assert = require('assert');
 
 describe('Find Last Index', () => {
-  it('finds the last index', () => {
-    const objects = [
-      {a: 0, b: 0},
-      {a: 1, b: 1},
-      {a: 2, b: 2},
-      {a: 0, b: 0}
-    ];
-    const result = findLastIndex(objects, {a: 0});
-    assert.equal(result, 3);
-  })
+    it('finds the last index', () => {
+        const objects = [
+            { a: 0, b: 0 },
+            { a: 1, b: 1 },
+            { a: 2, b: 2 },
+            { a: 0, b: 0 }
+        ];
+        const result = findLastIndex(objects, { a: 0 });
+        assert.equal(result, 3);
+    });
+    it('returns null if not found', () => {
+        const malcolm = [{ yes: 1, no: 0 }, { yes: 0, no: 1 }];
+        const result = findLastIndex(malcolm, { maybe: 1 });
+        assert.equal(result, null);
+    });
 });
-  
