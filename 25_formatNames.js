ce0a597f7d8362e23126ced315a9c4aba65ead85
totallynotleo <leodabrim@gmail.com>
2019-06-24 10:04:48 +1000

    Completed 25_formatNames

--- 25_formatNames.js
+++ 25_formatNames.js
@@ -29,40 +29,45 @@
 */
 
 function formatNames(names) {
-	// Your code goes here
+    names = names
+        .map(object => `(${object.last}, ${object.first})`)
+        .sort()
+        .join(', ')
+        .toUpperCase();
+    return names;
 }
 
-let assert = require("assert")
+let assert = require('assert');
 
-describe("formatNames", () => {
-	context("with an empty input", () => {
-		it("should return an empty string with empty input", () => {
-			assert.equal(formatNames([]), "")
-		})
-	})
-	context("with input", () => {
-		it("should return (ABEL, SARA), (JONES, SARA), (MILLER, JOE), (MILLER, JOHN) with input specified", () => {
-			assert.equal(
-				formatNames([
-					{
-						first: "Joe",
-						last: "Miller"
-					},
-					{
-						first: "John",
-						last: "Miller"
-					},
-					{
-						first: "Sara",
-						last: "Jones"
-					},
-					{
-						first: "Sara",
-						last: "Abel"
-					}
-				]),
-				"(ABEL, SARA), (JONES, SARA), (MILLER, JOE), (MILLER, JOHN)"
-			)
-		})
-	})
-})
+describe('formatNames', () => {
+    context('with an empty input', () => {
+        it('should return an empty string with empty input', () => {
+            assert.equal(formatNames([]), '');
+        });
+    });
+    context('with input', () => {
+        it('should return (ABEL, SARA), (JONES, SARA), (MILLER, JOE), (MILLER, JOHN) with input specified', () => {
+            assert.equal(
+                formatNames([
+                    {
+                        first: 'Joe',
+                        last: 'Miller'
+                    },
+                    {
+                        first: 'John',
+                        last: 'Miller'
+                    },
+                    {
+                        first: 'Sara',
+                        last: 'Jones'
+                    },
+                    {
+                        first: 'Sara',
+                        last: 'Abel'
+                    }
+                ]),
+                '(ABEL, SARA), (JONES, SARA), (MILLER, JOE), (MILLER, JOHN)'
+            );
+        });
+    });
+});
