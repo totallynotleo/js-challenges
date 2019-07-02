daceb07227036689c9bfcebc486754e7bebddf85
NikChao <nik.chaourov@gmail.com>
2019-07-03 09:59:23 +1000

    Added more complete testing to 31_cardMask

--- 31_cardMask.js
+++ 31_cardMask.js
@@ -28,3 +28,41 @@ describe('maskify', () => {
         assert.equal(maskify('123456789'), '#####6789');
     });
 });
+describe('maskify', () => {
+    it('should mask all but the last 4 - only 4', () => {
+        assert.equal(maskify('6789'), '6789');
+    });
+});
+describe('maskify', () => {
+    it('should return empty for empty', () => {
+        assert.equal(maskify(''), '');
+    });
+});
+describe('maskify', () => {
+    it('Example Case 1', () => {
+        assert.equal(maskify('1233333'), '###3333');
+    });
+});
+describe('maskify', () => {
+    it('Example Case 2', () => {
+        assert.equal(maskify('64607935616'), '#######5616');
+    });
+});
+describe('maskify', () => {
+    it('Example Case 3', () => {
+        assert.equal(maskify('1'), '1');
+    });
+});
+describe('maskify', () => {
+    it('Example Case 4', () => {
+        assert.equal(maskify('Skippy'), '##ippy');
+    });
+});
+describe('maskify', () => {
+    it('Example Case 5', () => {
+        assert.equal(
+            maskify('Nananananananananananananananana Batman!'),
+            '####################################man!'
+        );
+    });
+});
